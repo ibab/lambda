7afa21768e02253d82910cdeb771ecbc40e3844f
Igor Babuschkin <igor@babuschkin.de>
2013-01-20 18:15:29 +0100

    Implemented simple command line

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,29 @@
 
 module Main where
 
+import System.Console.Haskeline
+
+import Language.Lambda
 import Language.Lambda.Parser
 
 main = do
-  putStrLn $ either (show) (show) (parseLambda "(λx. x)")
+  putStrLn "Please enter a lambda expression."
+  runInputT defaultSettings repl
 
+repl :: InputT IO ()
+repl = do
+  input <- getInputLine "> "
+  case input of
+    Nothing -> return ()
+    Just "quit" -> return ()
+    Just input -> do displayReduction input
+                     repl
+
+displayReduction s = 
+  case parseLambda s of
+    Left err  -> outputStrLn $ show err
+    Right lam -> mapM_ (outputStrLn . (" -> "++) . show) (reduce lam)
+
+reduce l = case betaReduce l of
+  Nothing  -> l:[]
+  Just red -> l:reduce red
